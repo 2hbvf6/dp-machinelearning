2066839b3cb59e0dae2c90631e7c28a7fa70ebae
Ipsita saha <ipsitaasaha7809@gmail.com>
2025-10-17 10:57:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Laerning App')
 
 st.write('This ia app builds machine learning models !')
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df= pd.read_csv(' C:\Users\sahai\OneDrive\Desktop\habuda project\updated exel.csv.xlsx ')
+  df
