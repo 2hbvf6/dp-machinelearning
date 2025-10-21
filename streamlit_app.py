6461bd030c45d79cc5d598e76118cf827820fdee
Ipsita saha <ipsitaasaha7809@gmail.com>
2025-10-21 11:08:20 +0530

    Update formatting of data display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,10 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df= pd.read_csv(' https://raw.githubusercontent.com/2hbvf6/dp-machinelearning/refs/heads/master/Updated%20data_01_05_2025.csv ')
   df
-st.write('*X*')
+  
+st.write('**X**')
 X = df.drop('label', axis=1)
 X
-st.write('*Y*')
+st.write('**Y**')
 Y = df.label
 Y
