4f7f1714b1f8d275f1cbebe0d2b9d976c1f90d27
Ipsita saha <ipsitaasaha7809@gmail.com>
2025-10-21 11:06:27 +0530

    Fix indentation and formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df= pd.read_csv(' https://raw.githubusercontent.com/2hbvf6/dp-machinelearning/refs/heads/master/Updated%20data_01_05_2025.csv ')
   df
-  st.write('*X*')
+st.write('*X*')
 X = df.drop('label', axis=1)
 X
 st.write('*Y*')
