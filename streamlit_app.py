793bb80bd21827e5f1061494afc938e222a127a7
Ipsita saha <ipsitaasaha7809@gmail.com>
2025-10-21 11:12:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 st.title('🤖 Machine Laerning App')
 
 st.write('This ia app builds machine learning models !')
+
 with st.expander('Data'):
   st.write('**Raw Data**')
   df= pd.read_csv(' https://raw.githubusercontent.com/2hbvf6/dp-machinelearning/refs/heads/master/Updated%20data_01_05_2025.csv ')
